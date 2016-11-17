708d92d5b176f7d956a6ec491ff4c1e4a2b4e8f0
Vitalii Chepel <Vitalii_Chepel@epam.com>
2016-11-17 17:35:01 +0200

    Added info with shapes-squares

--- src/main/java/soft/course/My.java
+++ src/main/java/soft/course/My.java
@@ -1,8 +1,28 @@
 package soft.course;
 
-public class My{
+public class My {
 
-public static void main (String [] args){
-System.out.println("Hello, World!");
-}
+    public static void main(String[] args) {
+        hello("World");
+        hello("Alexei");
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " равняется " + area(l));
+
+        double rec = 7;
+        double rectwo = 9;
+        System.out.println("Площадь прямоугольника со сторонами " + rec + " и " +  rectwo +  " равняется " + area(rec,rectwo));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double lenka) {
+        return lenka * lenka;
+    }
+
+    public static double area(double rec, double rectwo) {
+        return rec * rectwo;
+    }
+
 }
